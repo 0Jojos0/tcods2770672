fcea7dddde70494be39dd3c3141c093a6ce53fca
0Jojos0 <Haquatom96@gmail.com>
2024-03-03 11:41:40 -0500

    añadida carpeta laravel

--- 03-sessions/config/database.php
+++ 03-sessions/config/database.php
@@ -1,5 +1,6 @@
 <?php
 
+    // - - - - - - - - - - - - - - - - - - - - 
     // Connection
     try {
         $conx = new PDO("mysql:host=".HOST.";dbname=".DBNAME."", USER, PASS);
@@ -12,6 +13,7 @@
         echo "Error: " . $e->getMessage();
     }
 
+    // - - - - - - - - - - - - - - - - - - - - 
     // login User
     function loginUser($conx, $email, $pass) {
         try {
@@ -24,7 +26,8 @@
             if ($stm->rowCount() > 0) {
                 $user = $stm->fetch();
                 if (password_verify($pass, $user['password'])) {
-                    $_SESSION['uid'] = $user['id'];
+                    $_SESSION['uid']   = $user['id'];
+                    $_SESSION['urole'] = $user['role'];
                     return true;
                 } else {
                     $_SESSION['error'] = "Email or Password incorrect, please try again";
@@ -40,4 +43,35 @@
         }
     }
 
-    
+    // - - - - - - - - - - - - - - - - - - - - 
+    // get Record
+    function getUser($conx, $id) {
+        try {
+            $sql = "SELECT * FROM users WHERE id = :id";
+            $stm = $conx->prepare($sql);
+            $stm->execute(['id' => $id]);
+            return $stm->fetch();
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+
+    // - - - - - - - - - - - - - - - - - - - - 
+    // Add User
+    function addUser($conx, $data) {
+        try {
+            $sql = "INSERT INTO users (document, fullname, photo, phone, email, password)
+                    VALUES (:document, :fullname, :photo, :phone, :email, :password)";
+            $smt = $conx->prepare($sql); 
+            
+            if ($smt->execute($data)) {
+                $_SESSION['msg'] = 'The ' . $data['fullname'] . ' user was registered successfully.' ;
+                return true;
+            } else {
+                return false;
+            } 
+
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
